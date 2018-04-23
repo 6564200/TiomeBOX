45194fa23bc84ec4b86bec60a65fe32a14f65594
Max K <maxus@list.ru>
2018-04-23 17:21:22 +0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,35 @@
+sudo apt-get update 
+sudo apt-get -y upgrade 
+sudo apt-get update 
+sudo apt-get -y install exfat-fuse
+#sudo mkdir /mnt/SD
+sudo apt-get -y install build-essential 
+sudo apt-get -y install python-dev
+sudo apt-get -y install python pip
+sudo pip install -U pip
+sudo pip install Adafruit_Python_DHT
+sudo apt-get -y install htop mc
+sudo apt-get -y install swig
+
+sudo apt-get -y install dnsmasq
+sudo apt-get -y install hostapd
+sudo apt-get -y install python-smbus
+sudo apt-get -y install i2c-tools
+
+sudo apt-get -y install libpng3
+sudo apt-get -y install libjpeg-dev 
+sudo apt-get -y install libjpeg8-dev 
+sudo apt-get -y install libfreetype6 
+sudo apt-get -y install libfreetype6-dev 
+sudo apt-get -y install zlib1g-dev 
+
+sudo apt-get -y install lirc
+sudo apt-get -y install libusb-dev
+
+sudo apt-get -y install libssl-dev 
+sudo apt-get -y install libffi-dev 
+sudo apt-get -y install python3-dev
+
+sudo pip install --upgrade setuptools
+sudo pip install cryptography
+sudo pip install paramiko
